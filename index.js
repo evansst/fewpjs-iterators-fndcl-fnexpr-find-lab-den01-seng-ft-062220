02a13f571a6479a84b2ac72d6a2f7bdc523e91d0
Sam Evans <evansst@icloud.com>
2020-07-17 14:56:07 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const result = array.find(superbowl => {
+    return superbowl.result == 'W';
+  });
+  return result ? result.year : undefined;
+}
